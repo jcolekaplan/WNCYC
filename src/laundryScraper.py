a1e88f4de2d4c8382e818ea0b12d022329cb41c9
jcolekaplan <31549019+jcolekaplan@users.noreply.github.com>
2017-10-25 20:13:53 -0400

    Update laundryScraper.py
    Generalized to all buildings. Scrapes all info and stores in Machine and Building objects.

--- src/laundryScraper.py
+++ src/laundryScraper.py
@@ -1,60 +1,76 @@
-"""Libraries for reading in webpages and parsing them"""
 import urllib3
 from bs4 import BeautifulSoup
 
 """Include building and machine classes"""
+from buildingsInfo import *
 from building import *
 from machine import *
 
 """Read in the LaundryView page for Barton Hall then use BeautifulSoup to convert it into a parsable object"""
 http = urllib3.PoolManager()
-barton = http.request('GET', 'http://classic.laundryview.com/classic_laundry_room_ajax.php?lr=3695891&adk=155604%27')
-soup = BeautifulSoup(barton.data, 'html.parser')
+url = 'http://classic.laundryview.com/classic_laundry_room_ajax.php?lr={}'
 
-"""Finds each row of the table with all the info for the given laundry room
-   Puts each row in a list
-"""
-table = soup.find_all('tr')
+"""Used to store Building and Machine objects with all the relevant info"""
+buildingList = list()
+machineDict = dict()
 
-"""Will hold all machines of type Machine once info is parsed"""
-bartonMachines = []
+"""Go through the buildings listed in buildingsInfo.py"""
+for buildingInfo in buildingsInfo:
+	"""Format correct URL and reads in web-page"""
+	laundryRoom = buildingInfo['roomNum']
+	building = http.request('GET', url.format(laundryRoom))
+	soup = BeautifulSoup(building.data, 'html.parser')
 
-"""Go through each row of the table
-   If the row contains div class="desc", then that row contains the machineNum, extract it
-   If the row contains alt="inuse" or alt="available" then it contains a .gif with the correct machineType, extract it
-   If the row contains div class="runtime", extract the runtime
-   Then, using all that info, create a Machine object, at it to Barton's list of machines
-"""
-for row in table:
-    
-    if row.find('div', 'desc'):
-        machineNum = row.find('div','desc').get_text().strip()
+	"""Finds each row of the table with all the info for the given laundry room
+	   Puts each row in a list
+	"""
+	table = soup.find_all('tr')
+	
+	"""Will use to check for machine duplicates"""
+	foundMachines = set()
+	
+	"""Initializes list of machines in that building"""
+	buildingId = buildingInfo['buildingId']
+	machineDict[buildingId] = list()
 
-    if row.find(alt={'inuse','available'}):
-        machineType = row.find(alt={'inuse', 'available'})
-        if str(machineType).find('washer') != -1:
-            machineType = 'Washer'
-        else:
-            machineType = 'Dryer'
-            
-    if row.find('div', 'runtime'):
-        runTime = row.find('div', 'runtime').get_text().strip()
-        
-        if runTime.find('remaining') != -1:
-            runTimeInt = [int(s) for s in runTime.split() if s.isdigit()]
-        else:
-            runTimeInt = 0
-            
-        machineId = machineType + '-' + machineNum
-        buildingId = 'Barton Hall-2 Floor'
-        if sum(p.machineId == machineId for p in bartonMachines) == 0:
-            bartonMachines.append(Machine(runTime, machineId, buildingId, machineType, runTimeInt, True))
+	"""Go through each row of the table
+	   If the row contains div class="desc", then that row contains the machineNum, extract it
+	   If the row contains alt="inuse" or alt="available" then it contains a .gif with the correct machineType, extract it
+	   If the row contains div class="runtime", extract the runtime
+	   Then, using all that info, create a Machine object, add it to the list of machines with key corresponding to buildingId
+	"""
+	for row in table:
+		if row.find('div', 'desc'):
+			machineNum = row.find('div','desc').get_text().strip()
+			
+		if row.find(alt={'inuse','available'}):
+			machineType = row.find(alt={'inuse', 'available'})
+			if str(machineType).find('washer') != -1:
+				machineType = 'Washer'
+			else:
+				machineType = 'Dryer'
+				
+		if row.find('div', 'runtime'):
+			runTime = row.find('div', 'runtime').get_text().strip()
+			if runTime.find('remaining') != -1:
+				runTimeInt = [int(s) for s in runTime.split() if s.isdigit()][0]
+			else:
+				runTimeInt = 0
+				
+			machineId = machineType + '-' + machineNum		
+			if machineId not in foundMachines:
+				foundMachines.add(machineId)
+				machineDict[buildingId].append((Machine(runTime, machineId, buildingId, machineType, runTimeInt, True)))
+	
+	"""Using data found above, create Building object
+	   Store in buildingList
+	"""
+	numWashers = sum(p.machineType == 'Washer' for p in machineDict[buildingId])
+	numDryers = sum(p.machineType == 'Dryer' for p in machineDict[buildingId])
+	buildingList.append(Building('success', buildingId, buildingInfo['friendlyNames'], numWashers, numDryers))
 
-"""Get the relevant info for Barton then create a Building object with all that info"""
-numWashers = sum(p.machineType == 'Washer' for p in bartonMachines)
-numDryers = sum(p.machineType == 'Dryer' for p in bartonMachines)
-nameList = ['Barton', 'Barton Hall', 'Hotel Barton']
-Barton = Building('success', 'Barton Hall-2 Floor', nameList, numWashers, numDryers)
-
-for machine in bartonMachines:
-    print (machine.machineId + ': ', machine.status)
+"""Test"""
+for building in buildingList:
+	print('\n{} #Washers={} #Dryers={}'.format(building.buildingId, building.numWashers, building.numDryers))
+	for machine in machineDict[building.buildingId]:
+		print('{}: {}'.format(machine.machineId, machine.status))
