616558a0bbbe24591c27a08d3b639fe046106ea7
jcolekaplan <31549019+jcolekaplan@users.noreply.github.com>
2017-11-04 12:49:33 -0400

    Update laundryScraper.py
    Writes to Dynamo table. Code separated into two main functions. One that scrapes laundryView and puts info into buildingList and machineDict. The other takes that info, uses another function to put into Dynamo format, then writes it to Dynamo table. Functions called at the end under 'if __name__ == "__main__":'
    Also, implemented environment variables for the credentials so no more parsing through config file.

--- src/laundryScraper.py
+++ src/laundryScraper.py
@@ -1,76 +1,152 @@
+import json
+import boto3
 import urllib3
+import configparser
 from bs4 import BeautifulSoup
 
 """Include building and machine classes"""
-from buildingsInfo import *
-from building import *
 from machine import *
+from building import *
+from buildingsInfo import *
 
-"""Read in the LaundryView page for Barton Hall then use BeautifulSoup to convert it into a parsable object"""
-http = urllib3.PoolManager()
-url = 'http://classic.laundryview.com/classic_laundry_room_ajax.php?lr={}'
+"""Convert all building and machine info to format compatible with DynamoDB tables"""
+def dictToItem(raw):
+	if type(raw) is dict:
+		resp = {}
+		for k,v in raw.items():
+			if type(v) is str:
+				resp[k] = {
+				        'S': v
+				}
+			elif type(v) is int:
+				resp[k] = {
+				    'N': str(v)
+				}
+			elif type(v) is dict:
+				resp[k] = {
+				    'N': dictToItem(v)
+				}
+			elif type(v) is list:
+				resp[k] = {'L': []}
+				for i in v:
+					resp[k]['L'].append(dictToItem(i))
+		
+		return resp
 
-"""Used to store Building and Machine objects with all the relevant info"""
-buildingList = list()
-machineDict = dict()
+	elif type(raw) is str:
+		return {
+		        'S': raw
+		}
+	elif type(raw) is int:
+		return {
+		        'N': str(raw)
+		}
 
-"""Go through the buildings listed in buildingsInfo.py"""
-for buildingInfo in buildingsInfo:
-	"""Format correct URL and reads in web-page"""
-	laundryRoom = buildingInfo['roomNum']
-	building = http.request('GET', url.format(laundryRoom))
-	soup = BeautifulSoup(building.data, 'html.parser')
-
-	"""Finds each row of the table with all the info for the given laundry room
-	   Puts each row in a list
-	"""
-	table = soup.find_all('tr')
+"""Scrape LaundryView for each building and all machines in each building
+   Put all the info into Building and Machine objects
+   Put all those objects into buildingList and machineDict
+"""   
+def scrapeLaundry():
+	"""Read in the LaundryView page for building then use BeautifulSoup to convert it into a parsable object"""
+	http = urllib3.PoolManager()
+	url = 'http://classic.laundryview.com/classic_laundry_room_ajax.php?lr={}'
 	
-	"""Will use to check for machine duplicates"""
-	foundMachines = set()
+	"""Go through the buildings listed in buildingsInfo.py"""
+	for buildingInfo in buildingsInfo:
+		"""Format correct URL and reads in web-page"""
+		laundryRoom = buildingInfo['roomNum']
+		building = http.request('GET', url.format(laundryRoom))
+		soup = BeautifulSoup(building.data, 'html.parser')
 	
-	"""Initializes list of machines in that building"""
-	buildingId = buildingInfo['buildingId']
-	machineDict[buildingId] = list()
-
-	"""Go through each row of the table
-	   If the row contains div class="desc", then that row contains the machineNum, extract it
-	   If the row contains alt="inuse" or alt="available" then it contains a .gif with the correct machineType, extract it
-	   If the row contains div class="runtime", extract the runtime
-	   Then, using all that info, create a Machine object, add it to the list of machines with key corresponding to buildingId
-	"""
-	for row in table:
-		if row.find('div', 'desc'):
-			machineNum = row.find('div','desc').get_text().strip()
-			
-		if row.find(alt={'inuse','available'}):
-			machineType = row.find(alt={'inuse', 'available'})
-			if str(machineType).find('washer') != -1:
-				machineType = 'Washer'
-			else:
-				machineType = 'Dryer'
+		"""Finds each row of the table with all the info for the given laundry room
+		   Puts each row in a list
+		"""
+		table = soup.find_all('tr')
+		
+		"""Will use to check for machine duplicates"""
+		foundMachines = set()
+		
+		"""Initializes list of machines in that building"""
+		buildingId = buildingInfo['buildingId']
+		machineDict[buildingId] = list()
+	
+		"""Go through each row of the table
+		   If the row contains div class="desc", then that row contains the machineNum, extract it
+		   If the row contains alt="inuse" or alt="available" then it contains a .gif with the correct machineType, extract it
+		   If the row contains div class="runtime", extract the runtime
+		   Then, using all that info, create a Machine object, add it to the list of machines with key corresponding to buildingId
+		"""
+		for row in table:
+			if row.find('div', 'desc'):
+				machineNum = row.find('div','desc').get_text().strip()
 				
-		if row.find('div', 'runtime'):
-			runTime = row.find('div', 'runtime').get_text().strip()
-			if runTime.find('remaining') != -1:
-				runTimeInt = [int(s) for s in runTime.split() if s.isdigit()][0]
-			else:
-				runTimeInt = 0
-				
-			machineId = machineType + '-' + machineNum		
-			if machineId not in foundMachines:
-				foundMachines.add(machineId)
-				machineDict[buildingId].append((Machine(runTime, machineId, buildingId, machineType, runTimeInt, True)))
-	
-	"""Using data found above, create Building object
-	   Store in buildingList
-	"""
-	numWashers = sum(p.machineType == 'Washer' for p in machineDict[buildingId])
-	numDryers = sum(p.machineType == 'Dryer' for p in machineDict[buildingId])
-	buildingList.append(Building(buildingId, buildingInfo['friendlyNames'], numWashers, numDryers))
+			if row.find(alt={'inuse','available'}):
+				machineType = row.find(alt={'inuse', 'available'})
+				if str(machineType).find('washer') != -1:
+					machineType = 'Washer'
+				else:
+					machineType = 'Dryer'
+					
+			if row.find('div', 'runtime'):
+				runTime = row.find('div', 'runtime').get_text().strip()
+				if runTime.find('remaining') != -1:
+					runTimeInt = [int(s) for s in runTime.split() if s.isdigit()][0]
+				else:
+					runTimeInt = 0
+					
+				machineId = buildingId + '-' + machineType + '-' + machineNum		
+				if machineId not in foundMachines:
+					foundMachines.add(machineId)
+					machineDict[buildingId].append((Machine(runTime, machineId, buildingId, machineType, runTimeInt, True)))
+		
+		"""Using data found above, create Building object
+		   Store in buildingList
+		"""
+		numWashers = sum(p.machineType == 'Washer' for p in machineDict[buildingId])
+		numDryers = sum(p.machineType == 'Dryer' for p in machineDict[buildingId])
+		buildingList.append(Building(buildingId, buildingInfo['friendlyNames'], numWashers, numDryers))
 
-"""Test"""
-for building in buildingList:
-	print('\n{} #Washers={} #Dryers={}'.format(building.buildingId, building.numWashers, building.numDryers))
-	for machine in machineDict[building.buildingId]:
-		print('{}: {}'.format(machine.machineId, machine.status))
+"""Iterate through buildingList and machineDict
+   Convert to dynamo format and write info to table
+"""
+def writeDynamo():
+	"""Client with access keys to write to Dynamo tables online"""
+	dynamodb = boto3.client('dynamodb',
+		  aws_access_key_id = AWS_ACCESS_KEY_ID,
+		  aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
+		  region_name = 'us-east-2')
+	"""Go through each building, convert it to dictionary, and put in relevant info in    
+	   Dynamo format
+	   Go through the machines for each of those buildings, do the same
+	   Then, write buildingTable and machineTable to Dynamo table
+	"""
+	for building in buildingList:
+		
+		buildingTable = dict()
+		machineTable = dict()
+		
+		buildingTable['Buildings'] = list()
+		machineTable['Machines'] = list()
+		
+		newBuilding = dict()
+		newBuilding['PutRequest'] = dict()
+		newBuilding['PutRequest']['Item'] = dictToItem(vars(building))
+		buildingTable['Buildings'].append(newBuilding)
+		
+		for machine in machineDict[building.buildingId]:
+			newMachine = dict()
+			newMachine['PutRequest'] = dict()
+			newMachine['PutRequest']['Item'] = dictToItem(vars(machine))
+			machineTable['Machines'].append(newMachine)
+	
+		buildingResponse = dynamodb.batch_write_item(RequestItems=buildingTable)
+		machineResponse = dynamodb.batch_write_item(RequestItems=machineTable)
+
+if __name__ == '__main__':
+	
+	"""Used to store Building and Machine objects with all the relevant info"""
+	buildingList = list()
+	machineDict = dict()
+
+	scrapeLaundry()
+	writeDynamo()
