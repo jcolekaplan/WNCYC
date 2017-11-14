74d7eac8bc95dda2c5b024df5437a87f868bb9f9
jcolekaplan <31549019+jcolekaplan@users.noreply.github.com>
2017-11-14 12:22:54 -0500

    Update getMachineId.py
    Verifies buildingId

--- src/getMachineId.py
+++ src/getMachineId.py
@@ -8,33 +8,37 @@ machineTable = dynamodb.Table('Machines')
 
 """Lambda handler function for /buildings/{buildingId}/machines/{machineId} API call
    Returns machine with the machineId specified in the path
+   or 'Building not found' error if buildingId doesn't match buildingId of machine
    or 'Machine not found' error if machineId not found by get_item search
 """
 def getMachineId(event, context):
     """If machineId specified, assign it to variable,
        use get_item to find it in machine table
-       put it in JSON format and return
+       if it finds a machine, verify it's in building specified in path
+	   if it is return machine info in JSON format
     """
+    buildingId = None
     machineId = None
     if event.get('pathParameters'):
+        buildingId = event.get('pathParameters').get('buildingId')
         machineId = event.get('pathParameters').get('machineId')
         response = machineTable.get_item(Key={'machineId': machineId})
         if response.get('Item'):
-            return {
-                'statusCode': 200,
-                'headers': {'Content-Type': 'application/json'},
-                'body': json.dumps(response['Item'], cls=DecimalEncoder)
-            }
+            if response['Item']['buildingId']==buildingId:
+                return {
+                    'statusCode': 200,
+                    'headers': {'Content-Type': 'application/json'},
+                    'body': json.dumps(response['Item'], cls=DecimalEncoder)
+                }
+            else:
+                return {
+                    'statusCode': 404,
+                    'headers': {'Content-Type': 'application/json'},
+                    'body': json.dumps({'error': 'Building not found'})
+                }
         else:
-            """Error if not found"""
             return {
                 'statusCode': 404,
                 'headers': {'Content-Type': 'application/json'},
                 'body': json.dumps({'error': 'Machine not found'})
             }
-    """Error if path not specified"""
-    return {
-        'statusCode': 404,
-        'headers': {'Content-Type': 'application/json'},
-        'body': json.dumps({'error': 'Path not found'})
-    }
