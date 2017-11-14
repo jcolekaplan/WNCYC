4eae65bd1c813dd93e96a0c64138eafdc403943c
jcolekaplan <31549019+jcolekaplan@users.noreply.github.com>
2017-11-14 12:24:40 -0500

    Update getMachines.py
    Uses filter expression in scan within handler to reduce latency.

--- src/getMachines.py
+++ src/getMachines.py
@@ -1,51 +1,54 @@
 import boto3
 from decEncoder import *
+from boto3.dynamodb.conditions import Key, Attr
 
 """Scan dynamo table"""
 dynamodb = boto3.resource('dynamodb', region_name = 'us-east-2')
 machineTable = dynamodb.Table('Machines')
-response = machineTable.scan()
 
 """Lambda handler function for /buildings/{buildingId}/machines API call
-   Returns all the machines in a given building if no queries specified
-   If query (machineType and/or status) specified, filter machines matching those query parameters
-   If no buildingId or queries are specified, return all machines
+   Returns all the machines in a given building
+   or machines matching query parameters (machineType and/or status)
+   or 'Building or machine not found' error if no machines are returned matching criteria
 """
 def getMachines(event, context):
-
     buildingId = None
     if event.get('pathParameters'):
         buildingId = event.get('pathParameters').get('buildingId')
-	
-    machineType = None
-    status = None
-    if event.get('queryStringParameters'):
-	    machineType = event.get('queryStringParameters').get('machineType')
-	    status = event.get('queryStringParameters').get('status')
-
     if buildingId:
-        machines = list()
-        for machine in response['Items']:
-            if buildingId == machine['buildingId']:
-                if not machineType and not status:
-                    machines.append(machine)
-                elif machineType and machine['machineType']==machineType and not status:
-                    machines.append(machine)
-                elif status and machine['status']==status and not machineType:
-                    machines.append(machine)
-                elif machineType and status and machine['machineType']==machineType and machine['status']==status:
-                    machines.append(machine)
-
-        return {
-            'statusCode': 200,
-            'headers': {'Content-Type': 'application/json'},
-            'body': json.dumps(machines, cls=DecimalEncoder)
-        }
- 
-
-    else:
-        return {
-            'statusCode': 200, 
-            'headers': {'Content-Type': 'application/json'},
-            'body': json.dumps(response["Items"], cls=DecimalEncoder)
-        }
+        machineType = None
+        status = None
+        if event.get('queryStringParameters'):
+	        machineType = event.get('queryStringParameters').get('machineType')
+	        status = event.get('queryStringParameters').get('status')
+			
+    	response = machineTable.scan(
+	    	FilterExpression=Attr('buildingId').eq(buildingId)
+		)
+        elif machineType and not status:
+            response = machineTable.scan(
+                FilterExpression=(Attr('buildingId').eq(buildingId)
+			    & Attr('machineType').eq(machineType))
+            )
+        elif status and not machineType:
+            response = machineTable.scan(
+                FilterExpression=(Attr('buildingId').eq(buildingId)
+			    & Attr('status').eq(status))
+            )
+        elif status and machineType:
+            response = machineTable.scan(
+                FilterExpression=(Attr('buildingId').eq(buildingId)
+                & Attr('machineType').eq(machineType)
+    			& Attr('status').eq(status))
+	    )
+        if response.get('Items'):
+            return {
+                'statusCode': 200,
+                'headers': {'Content-Type': 'application/json'},
+                'body': json.dumps(response['Items'], cls=DecimalEncoder)
+            }
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'application/json'},
+        'body': json.dumps({'error': 'Building or machines not found'})
+    }
