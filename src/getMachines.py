ed456991f85d40eed5679fcd7fe341d34dab6be8
jcolekaplan <31549019+jcolekaplan@users.noreply.github.com>
2017-11-15 13:34:41 -0500

    Update getMachines.py
    Fixed logic issue with filters

--- src/getMachines.py
+++ src/getMachines.py
@@ -10,45 +10,40 @@ machineTable = dynamodb.Table('Machines')
    Returns all the machines in a given building
    or machines matching query parameters (machineType and/or status)
    or 'Building or machine not found' error if no machines are returned matching criteria
+   or 'No path parameters' if there are no path parameters
 """
 def getMachines(event, context):
     buildingId = None
     if event.get('pathParameters'):
         buildingId = event.get('pathParameters').get('buildingId')
-    if buildingId:
-        machineType = None
-        status = None
+        filterExpression=Attr('buildingId').eq(buildingId)
+        acceptedFilters = ['machineType', 'status']
+        
         if event.get('queryStringParameters'):
-	        machineType = event.get('queryStringParameters').get('machineType')
-	        status = event.get('queryStringParameters').get('status')
-			
-    	response = machineTable.scan(
-	    	FilterExpression=Attr('buildingId').eq(buildingId)
-		)
-        elif machineType and not status:
-            response = machineTable.scan(
-                FilterExpression=(Attr('buildingId').eq(buildingId)
-			    & Attr('machineType').eq(machineType))
-            )
-        elif status and not machineType:
-            response = machineTable.scan(
-                FilterExpression=(Attr('buildingId').eq(buildingId)
-			    & Attr('status').eq(status))
-            )
-        elif status and machineType:
-            response = machineTable.scan(
-                FilterExpression=(Attr('buildingId').eq(buildingId)
-                & Attr('machineType').eq(machineType)
-    			& Attr('status').eq(status))
+	        for key in event.get('queryStringParameters'):
+	            if key in acceptedFilters:
+	                filterExpression = filterExpression & Attr(key).eq(event.get('queryStringParameters').get(key))
+	    
+        response = machineTable.scan(
+	        FilterExpression=filterExpression
 	    )
+
         if response.get('Items'):
             return {
                 'statusCode': 200,
                 'headers': {'Content-Type': 'application/json'},
                 'body': json.dumps(response['Items'], cls=DecimalEncoder)
             }
-    return {
-        'statusCode': 200,
-        'headers': {'Content-Type': 'application/json'},
-        'body': json.dumps({'error': 'Building or machines not found'})
-    }
+        else:
+            return {
+                'statusCode': 200,
+                'headers': {'Content-Type': 'application/json'},
+                'body': json.dumps({'error': 'Building or machines not found'})
+            }
+    else:
+        return {
+            'statusCode': 400,
+            'headers': {'Content-Type': 'application/json'},
+            'body': json.dumps({'error': 'No path parameters'})
+
+        }
