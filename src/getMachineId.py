38a6c8e554b7f75924a7a9335254313210434381
jcolekaplan <31549019+jcolekaplan@users.noreply.github.com>
2017-11-15 13:39:17 -0500

    Update getMachineId.py
    Added corresponding else statement to if. Made more logically consistent.

--- src/getMachineId.py
+++ src/getMachineId.py
@@ -8,23 +8,21 @@ machineTable = dynamodb.Table('Machines')
 
 """Lambda handler function for /buildings/{buildingId}/machines/{machineId} API call
    Returns machine with the machineId specified in the path
-   or 'Building not found' error if buildingId doesn't match buildingId of machine
+   or 'Building not found' if buildingId in machine found does not match buildingId in path
    or 'Machine not found' error if machineId not found by get_item search
+   or 'No path parameters' if there are no path parameters
 """
 def getMachineId(event, context):
     """If machineId specified, assign it to variable,
        use get_item to find it in machine table
-       if it finds a machine, verify it's in building specified in path
-	   if it is return machine info in JSON format
+       put it in JSON format and return
     """
-    buildingId = None
-    machineId = None
     if event.get('pathParameters'):
         buildingId = event.get('pathParameters').get('buildingId')
         machineId = event.get('pathParameters').get('machineId')
         response = machineTable.get_item(Key={'machineId': machineId})
         if response.get('Item'):
-            if response['Item']['buildingId']==buildingId:
+            if response.get('Item').get('buildingId')==buildingId:
                 return {
                     'statusCode': 200,
                     'headers': {'Content-Type': 'application/json'},
@@ -37,8 +35,16 @@ def getMachineId(event, context):
                     'body': json.dumps({'error': 'Building not found'})
                 }
         else:
+            """Error if not found"""
             return {
                 'statusCode': 404,
                 'headers': {'Content-Type': 'application/json'},
                 'body': json.dumps({'error': 'Machine not found'})
             }
+    else:
+        """No path parameters"""
+        return {
+            'statusCode': 400,
+            'headers': {'Content-Type': 'application/json'},
+            'body': json.dumps({'error': 'No path parameters'})
+        }
