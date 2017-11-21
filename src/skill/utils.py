12aebc68c42ddc1162e3611b506ca8e2fc6c1a4f
jcolekaplan <31549019+jcolekaplan@users.noreply.github.com>
2017-11-21 17:33:32 -0500

    Create utils.py

---
+++ src/skill/utils.py
@@ -0,0 +1,38 @@
+import json
+import urllib3
+
+""" --------------- Helpers that build all of the responses ---------------------- """
+def buildSpeechletResponse(title, output, repromptText, shouldEndSession):
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
+        },
+        'card': {
+            'type': 'Simple',
+            'title': 'SessionSpeechlet - ' + title,
+            'content': 'SessionSpeechlet - ' + output
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': repromptText
+            }
+        },
+        'shouldEndSession': shouldEndSession
+    }
+
+def buildResponse(sessionAttributes, speechletResponse):
+    return {
+        'version': '1.0',
+        'sessionAttributes': sessionAttributes,
+        'response': speechletResponse
+    }
+    
+""" --------------- Get info from API ------------- """
+def callApi(url):
+       http = urllib3.PoolManager()
+       r = http.request('GET', url)
+       data = r.data
+       jsonData = json.loads(data)
+       return jsonData
