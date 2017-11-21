6be0e5a9c39f15bbf6972c58e851e2f46562122a
jcolekaplan <31549019+jcolekaplan@users.noreply.github.com>
2017-11-21 17:34:13 -0500

    Create setBuildingIntent.py

---
+++ src/skill/setBuildingIntent.py
@@ -0,0 +1,45 @@
+import boto3
+from utils import *
+
+"""Dynamo resource and table"""
+dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
+table = dynamodb.Table('Users')
+
+def createBuildingAttributes(building):
+    return {'building': building}
+
+def setBuildingInSession(intent, session):
+    """ Sets the building in the session and prepares the speech to reply to the user."""
+    cardTitle = intent.get('name')
+    sessionAttributes = {}
+    shouldEndSession = False
+
+    if 'building' in intent.get('slots'):
+        building = intent.get('slots').get('building').get('value')
+        sessionAttributes = createBuildingAttributes(building)
+        speechOutput = 'I now know your building is ' + building.capitalize() + '. ' \
+                       'You can ask me what your building is by saying, what is my building?'
+        repromptText = 'You can ask me what your building is by saying, what is my building?'
+        
+        """Calls /buildings?friendlyName= API Call to find valid buildingId"""
+        findBuilding = callApi(
+            "https://go3ba09va5.execute-api.us-east-2.amazonaws.com/Test/buildings?friendlyName={}".format(building)
+        )
+        
+        if findBuilding.get('buildingId'):
+            userInfo = {
+                'userId' : session.get('user').get('userId'),
+                'buildingId' : findBuilding.get('buildingId')
+            }
+            table.put_item(Item=userInfo)
+        else:
+            speechOutput = 'I could not find that building. Please try again.'\
+                           'You can tell me your building by saying, my building is.'
+            repromptText = 'You can tell me your building by saying, my building is.'
+        
+    else:
+        speechOutput = 'I am not sure what your building is. Please try again.'
+        repromptText = 'I am not sure what your building is. Please try again.' \
+                       'You can tell me your building by saying, my building is.'
+    return buildResponse(sessionAttributes, buildSpeechletResponse(
+        cardTitle, speechOutput, repromptText, shouldEndSession))
