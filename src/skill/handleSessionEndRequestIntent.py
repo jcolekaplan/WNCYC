8eb9f95f2e5b26f453eec0b43470d7352d492497
jcolekaplan <31549019+jcolekaplan@users.noreply.github.com>
2018-03-25 13:08:11 -0400

    Delete handleSessionEndRequestIntent.py

--- src/skill/handleSessionEndRequestIntent.py
+++
@@ -1,8 +0,0 @@
-from utils import *
-
-def handleSessionEndRequest():
-    cardTitle = 'Session Ended'
-    shouldEndSession = True
-    speechOutput, repromptText = endResponse()
-    return buildResponse({}, buildSpeechletResponse(
-        cardTitle, speechOutput, repromptText, shouldEndSession))
