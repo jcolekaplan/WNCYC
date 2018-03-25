0b66b0ea332bacf27b1fa9c0ae76c2a8a1858d6f
jcolekaplan <31549019+jcolekaplan@users.noreply.github.com>
2018-03-25 13:08:03 -0400

    Delete getWelcomeResponseIntent.py

--- src/skill/getWelcomeResponseIntent.py
+++
@@ -1,9 +0,0 @@
-from utils import *
-
-def getWelcomeResponse():
-    sessionAttributes = {}
-    cardTitle = 'Welcome'
-    shouldEndSession = False
-    speechOutput, repromptText = welcomeResponse()
-    return buildResponse(sessionAttributes, buildSpeechletResponse(
-        cardTitle, speechOutput, repromptText, shouldEndSession))
