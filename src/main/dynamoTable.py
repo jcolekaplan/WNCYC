5fd1fca7476fd827e1423e987412bcc633b21d66
jcolekaplan <31549019+jcolekaplan@users.noreply.github.com>
2018-04-06 21:14:47 -0400

    Add dynamoTable class
    Don't know why this wasn't here before...

---
+++ src/main/dynamoTable.py
@@ -0,0 +1,34 @@
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+
+class DynamoTable(object):
+    
+    """Initialize table
+       Can be buildings, machines, users
+    """
+    def __init__(self, tableType):
+        allowedTableTypes = ['Buildings', 'Machines', 'Users']
+        if tableType not in allowedTableTypes:
+            raise ValueError('Invalid table type! Must be one of: {}'.format(allowedTableTypes))
+        dynamodb = boto3.resource('dynamodb', region_name = 'us-east-2')
+        self.table = dynamodb.Table(tableType)
+
+    def put(self, Info):
+        self.table.put_item(Item=vars(Info))
+    
+    def get(self, **kwargs):
+        
+        if len(kwargs) != 1:
+            raise ValueError('No key found or too many given')
+        
+        else:
+            keyStr = list(kwargs.keys())[0]
+            keyInfo = kwargs.get(keyStr)
+            getInfo = self.table.get_item(Key={keyStr: keyInfo})
+            return getInfo
+    
+    def scan(self):
+        return self.table.scan()
+    
+    def scanf(self, filterExpression):
+        return self.table.scan(FilterExpression=filterExpression)
